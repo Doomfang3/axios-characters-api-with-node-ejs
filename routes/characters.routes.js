3b9d47b9cf2defcd1034c4de5215861b3e369eee
Iskandar Froede <iskandar.lucyane@gmail.com>
2022-11-02 15:47:43 +0100

    create done

--- routes/characters.routes.js
+++ routes/characters.routes.js
@@ -4,11 +4,34 @@ const axios = require("axios");
 /* GET home page */
 router.get("/characters", (req, res, next) => {
     axios.get("https://ih-crud-api.herokuapp.com/characters")
-    .then(responseFromAPI => {
+    .then((responseFromAPI) => {
         // console.log(responseFromAPI)
         res.render("characters/list-characters", { characters: responseFromAPI.data });
     })
     .catch(err => console.error(err))
+});
+
+// GET new characters
+router.get("/characters/create", (req, res) => {
+        res.render("characters/create-character");
+});
+
+//POST new characters
+router.post("/characters/create", (req, res, next) => {
+
+    if (req.body.debt === "on") {
+        req.body.debt = true
+    } else {
+        req.body.debt = false
+    }
+
+    axios.post("https://ih-crud-api.herokuapp.com/characters", req.body) // req.body for displaying the content 
+    .then ((responseFromAPI) => {
+        // console.log(responseFromAPI)
+        res.redirect("/characters");
+    })
+    .catch(err => console.error(err))
+    console.log(req.body)
 });
 
 
@@ -20,6 +43,7 @@ router.get("/characters/:id", (req, res, next) => {
     })
     .catch(err => console.error(err))
 });
+
 
 module.exports = router;
 
